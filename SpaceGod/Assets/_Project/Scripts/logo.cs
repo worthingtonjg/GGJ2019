b38ff2c84eb5c433466a05192d5d67e04258e0a7
coltQrow <worthingtonrow@gmail.com>
2019-01-26 15:58:15 -0700

    stuff

---
+++ SpaceGod/Assets/_Project/Scripts/logo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class logo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(next());
+    }
+
+    private IEnumerator next()
+    {
+        yield return new WaitForSeconds(3f);
+        SimpleSceneFader.ChangeSceneWithFade("MainMenu");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
