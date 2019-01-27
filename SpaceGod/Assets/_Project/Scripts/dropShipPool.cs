6b2cebfd4ff9980528826c35cddca3ea332050a3
Jon Worthington <worthingtonjg@gmail.com>
2019-01-26 19:19:57 -0700

    fixing camera

---
+++ SpaceGod/Assets/_Project/Scripts/dropShipPool.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dropShipPool : MonoBehaviour
+{
+    public static List<GameObject> pool = new List<GameObject>();
+   
+}
