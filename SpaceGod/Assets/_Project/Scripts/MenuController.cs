b77f1a1b06c5654f79f02e3b2725fa8a6e3d6eb5
Jon Worthington <worthingtonjg@gmail.com>
2019-01-27 15:59:48 -0700

    models

--- SpaceGod/Assets/_Project/Scripts/MenuController.cs
+++ SpaceGod/Assets/_Project/Scripts/MenuController.cs
@@ -11,24 +11,4 @@ public class MenuController : MonoBehaviour
         GameObject player = GameObject.Find("Player");
         //player.transform.position = new Vector3(-41f, 52f, -176f);
     }
-
-    public void TargetHit(object name)
-    {
-        print("Target Hit Method");
-        if(name.ToString() == "StartGame")
-        {
-            SceneManager.LoadScene("MainScene");
-        }
-
-        if (name.ToString() == "ModelViewer")
-        {
-            SceneManager.LoadScene("01rocketmodel");
-        }
-
-        if (name.ToString() == "Credits")
-        {
-            SceneManager.LoadScene("Credits");
-        }
-
-    }
 }
