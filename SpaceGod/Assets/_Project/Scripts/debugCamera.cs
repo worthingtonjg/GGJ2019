1b9943ee7481ebc0c444f1b0c1f8fa2c6a500bf4
Jonathan Wortington <worthingtonjg@gmail.com>
2019-01-29 20:58:24 -0700

    shortcut keys

---
+++ SpaceGod/Assets/_Project/Scripts/debugCamera.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class debugCamera : MonoBehaviour
+{
+    GameObject player;
+    GameObject debugCam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        if(player != null)
+        {
+            this.gameObject.SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyUp(KeyCode.Z))
+        {
+            this.gameObject.SetActive(true);
+            player.SetActive(false);
+        }
+    }
+}
