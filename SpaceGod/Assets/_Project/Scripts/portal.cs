a34731e224adc994a58d00ce4ccb84fb3d7a1f33
Jonathan Wortington <worthingtonjg@gmail.com>
2019-01-29 22:52:09 -0700

    fixing portal

--- SpaceGod/Assets/_Project/Scripts/portal.cs
+++ SpaceGod/Assets/_Project/Scripts/portal.cs
@@ -5,16 +5,23 @@ using UnityEngine.SceneManagement;
 
 public class portal : MonoBehaviour
 {
+    public GameObject PortalPrefab;
+
     // Start is called before the first frame update
     void Start()
     {
+        var player = GameObject.FindGameObjectWithTag("Player");
+        
+        Vector3 position = new Vector3(player.transform.position.x, player.transform.position.y, player.transform.position.z + 10);
+        
+        GameObject.Instantiate(PortalPrefab, position, player.transform.rotation);
         StartCoroutine(NextLevel());
     }
 
     IEnumerator NextLevel()
     {
         yield return new WaitForSeconds(3);
-        SceneManager.LoadScene("Home");
+        //SceneManager.LoadScene("Home");
     }
 
 }
